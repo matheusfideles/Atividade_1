b7f92352c0fc7e80eae622e3ea8869606de2004b
matheusfideles <matheus.fideles02@gmail.com>
2024-05-13 13:59:44 -0300

    Teste

--- src/main.jl
+++ src/main.jl
@@ -2,6 +2,6 @@ include("test.jl")
 #Função que executa os testes - precisa que passe as dimensões de teste
 #Salva os gráficos para o problema simétrico na pasta images
 function main()
-    dim=[2,5,10,50,100,250,500,750,1000,2500,5000,7500,8000,8500,9000,9500,10000]
+    dim=[2,5,10]
     plot_lines(dim)
 end
