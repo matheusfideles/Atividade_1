16857337245f868061fe99f2b06eb16f27987520
matheusfideles <matheus.fideles02@gmail.com>
2024-05-28 16:21:51 -0300

    Códigos não mudaram muito - precisa ajustar a dimensão e quais gráficos para colocar no relatório

--- src/main.jl
+++ src/main.jl
@@ -2,7 +2,7 @@ include("test.jl")
 #Função para plotar todos os gráficos
 function main()
     #dim=[2,5,10,100,250,500,750,1000,2000,4000,6000,8000,10000]
-    dim=[2,5,10,100,250,500,750]
+    dim=[i for i=100:100:900]
 
     #Tempo da Inversa - todos os problemas
     plot_times_all(dim,"inv")
@@ -12,10 +12,16 @@ function main()
         #Tempos dos demais métodos - LU, Chol e PLU
         plot_times(i,dim,["plu","lu","chol"])
 
-        #Erros para cada problema - tudo menos inversa
-        scatter_error(i,dim,["plu","lu","chol"])
+        #Erros para cada problema
+        scatter_error(i,dim,["plu","chol"])
+        scatter_error(i,dim,["lu"])
 
         #Resíduos para cada problema - tudo menos inversa
-        scatter_residue(i,dim,["plu","lu","chol"])
+        scatter_residue(i,dim,["plu","chol"])
+        scatter_residue(i,dim,["lu"])
+
+        #Erro e residuo para inversa
+        scatter_error(i,dim,["inv"])
+        scatter_residue(i,dim,["inv"])
     end
 end
